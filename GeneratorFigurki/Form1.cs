3a18d807707860764f7765eccca208163b95e5c3
velikow <83334334+velikow@users.noreply.github.com>
2023-01-20 17:26:48 +0200

    Figurki da rabotqt i oshte gluposti

--- GeneratorFigurki/Form1.cs
+++ GeneratorFigurki/Form1.cs
@@ -45,12 +45,114 @@ namespace GeneratorFigurki
         {
             while (isSquareGenerated)
             {
-                
+                GenerateSquare();
                 Thread.Sleep(100);
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void triangleBtn_Click(object sender, EventArgs e)
+        {
+            triangleThread = new Thread(TriangleThread);
+            isTriangleGenerated = true;
+            triangleThread.Start();
+        }
+        private void TriangleThread()
+        {
+            while (isTriangleGenerated)
+            {
+                GenerateTriangle();
+                Thread.Sleep(100);
+            }
+        }
+
+        private void circleBtn_Click(object sender, EventArgs e)
+        {
+            circleThread = new Thread(CircleThread);
+            isCircleGenerated = true;
+            circleThread.Start();
+        }
+        private void CircleThread()
+        {
+            while (isCircleGenerated)
+            {
+                GenerateCircle();
+                Thread.Sleep(100);
+            }
+         
+        }
+
+        private void stopGeneratingBtn_Click(object sender, EventArgs e)
+        {
+            isSquareGenerated = false;
+            isTriangleGenerated = false;
+            isCircleGenerated = false;
+        }
+        private Brush PickBrush()
+        {
+            Brush result = Brushes.Transparent;
+            Type brushesType = typeof(Brushes);
+            PropertyInfo[] properties = brushesType.GetProperties();
+            int randomBrush = random.Next(properties.Length);
+            result = (Brush)properties[randomBrush].GetValue(null, null);
+            return result;
+        }
+
+        private void GenerateSquare()
+        {
+            int size;
+            GetValues();
+            if (panelYDiff < panelXDiff)
+            {
+                size = panelYDiff / 4;
+            }
+            else
+            {
+                size = panelXDiff / 4;
+            }
+            g.FillRectangle(PickBrush(), panelX, panelY, size, size);
+        }
+
+        private void GenerateTriangle()
+        {
+            int size;
+            GetValues();
+            if (panelYDiff < panelXDiff)
+            {
+                size = panelYDiff / 6;
+            }
+            else
+            {
+                size = panelXDiff / 6;
+            }
+            FillTriangle(new Point(panelX, panelY), size);
+        }
+
+        private void GenerateCircle()
+        {
+            int size;
+            GetValues();
+            if (panelYDiff < panelXDiff)
+            {
+                size = panelYDiff / 4;
+            }
+            else
+            {
+                size = panelXDiff / 4;
+            }
+            g.FillEllipse(PickBrush(), panelX, panelY, size, size);
+        }
+        private void FillTriangle(Point p, int size)
+        {
+            Graphics g = CreateGraphics();
+            g.FillPolygon(PickBrush(), new Point[] { p, new Point(p.X - size, p.Y + (int)(size * Math.Sqrt(3))), new Point(p.X + size, p.Y + (int)(size * Math.Sqrt(3))) });
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
         {
 
         }
