78c52459f6737fc5b50f675bec195234a4c170e2
velikow <83334334+velikow@users.noreply.github.com>
2023-01-20 17:19:42 +0200

    Brussh

--- GeneratorFigurki/Form1.Designer.cs
+++ GeneratorFigurki/Form1.Designer.cs
@@ -85,6 +85,7 @@
             this.Controls.Add(this.squareBtn);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
             this.ResumeLayout(false);
 
         }
